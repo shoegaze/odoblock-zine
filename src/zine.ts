ca4f48fed5203fd29db6adb1f93788addb60a08d
spike <bank.tellr@gmail.com>
2022-06-05 17:25:23 +0900

    Adds a test demo for THREE.js

--- src/zine.ts
+++ src/zine.ts
@@ -1,10 +1,37 @@
 import * as THREE from 'three'
 
-(function debug() {
-  console.log("Hello, world!")
 
-  const container = document.querySelector('#container')
-  if (container) {
-    container.textContent = "Hello, world!"
+const main = () => {
+  // Setup
+  const scene = new THREE.Scene()
+  const cam = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+  cam.position.z = 5
+
+  const renderer = new THREE.WebGLRenderer()
+
+  renderer.setSize(window.innerWidth, window.innerHeight)
+  document.body.appendChild(renderer.domElement)
+
+  // Test scene
+  const geometry = new THREE.BoxGeometry(1, 1, 1)
+  const material = new THREE.MeshBasicMaterial({
+    color: 0xff00ff
+  })
+  const cube = new THREE.Mesh(geometry, material)
+
+  scene.add(cube)
+
+  // Animation loop
+  const animate = () => {
+    requestAnimationFrame(animate)
+
+    cube.rotation.x += 0.01
+    cube.rotation.y += 0.01
+
+    renderer.render(scene, cam)
   }
-})()
+
+  animate()
+}
+
+main()
