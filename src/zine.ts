c0fbf5ee24ea3e53591b01fd6bb03f8a2b8131c8
spike <bank.tellr@gmail.com>
2022-06-05 17:18:10 +0900

    Adds typescript entrypoint

---
+++ src/zine.ts
@@ -0,0 +1,10 @@
+import * as THREE from 'three'
+
+(function debug() {
+  console.log("Hello, world!")
+
+  const container = document.querySelector('#container')
+  if (container) {
+    container.textContent = "Hello, world!"
+  }
+})()
