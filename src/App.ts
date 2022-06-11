359263a8a99cbb86067d2237187c789e1bfe1922
spike <bank.tellr@gmail.com>
2022-06-12 04:42:01 +0900

    Makes `App` use the `AppCameraDragger` class

--- src/App.ts
+++ src/App.ts
@@ -1,5 +1,8 @@
 import * as THREE from "three"
+import { clamp, lerp } from "three/src/math/MathUtils"
 import { AnimatedScene } from "./AnimatedScene"
+import AppCameraDragger from "./AppCameraDragger"
+import Physics from "./Physics"
 
 
 type AppAnimatedSceneMethod = (this: App, scene: AnimatedScene) => void
@@ -10,21 +13,22 @@ export interface App {
   renderer: THREE.WebGLRenderer
   animatedScenes: Array<AnimatedScene>
   clock: THREE.Clock
+  cameraDragger: AppCameraDragger
 
   addScene: AppAnimatedSceneMethod
+  startPhysics: AppMethod
   startAnimation: AppMethod
   resize: AppMethod
   render: AppMethod
 
-  translate: (dx: number, dy: number, sensitivity: number) => void,
-  zoom: (dz: number, sensitivity: number) => void
+  queueTranslation: (dx: number, dy: number) => void
+  queueZoom: (zoom: number) => void
 }
 
 export const createApp = (canvas: HTMLCanvasElement): App => {
   const { innerWidth: w, innerHeight: h } = window
   const s = Math.min(w, h)
 
-  // Perspective camera:
   const cam = new THREE.PerspectiveCamera(
     45,         // fov
     1.0,        // aspect
@@ -44,18 +48,73 @@ export const createApp = (canvas: HTMLCanvasElement): App => {
     checkShaderErrors: true
   }
 
+  let queuedTranslation: THREE.Vector2 | null = null
+  let queuedZoom: number | null = null
+
   return {
     cam,
     renderer,
     animatedScenes: [],
     clock: new THREE.Clock(false),
+    cameraDragger: new AppCameraDragger(cam),
 
-    addScene(this: App, as: AnimatedScene) {
+    addScene(as: AnimatedScene) {
       as.setup(this)
       this.animatedScenes.push(as)
     },
 
-    startAnimation(this: App) {
+    startPhysics() {
+      this.cameraDragger.startPhysicsLoop(
+        (physics: Physics, _) => {
+          { // Clamp speed of velocity
+            // TODO: Hoist this to AppCameraDragger
+            const maxSpeed = 50.0
+            physics.velocity.clampLength(0, maxSpeed)
+          }
+
+          { // Apply queued inputs as a force
+            const force = new THREE.Vector3(
+              queuedTranslation?.x ?? 0.0,
+              queuedTranslation?.y ?? 0.0,
+              queuedZoom ?? 0.0
+            )
+
+            physics.addForce(force)
+
+            // Reset force to be applied next step
+            queuedTranslation = null
+            queuedZoom = null
+          }
+        },
+        (physics: Physics, _) => {
+          // TODO: Implement deceleration after receiving no input for t seconds
+
+          { // DEBUG: Naive deceleration towards 0
+            const s = 0.9
+
+            const a = physics.acceleration.clone()
+            const f = a.negate()
+              .multiplyScalar(s)
+            // .multiplyScalar(clamp(dt, 0.0, 1.0))
+            // .multiplyScalar(physics.mass)
+
+            physics.addForce(f)
+            physics.velocity.multiplyScalar(s)
+          }
+
+          // { // DEBUG: Log physical parameters
+          //   console.groupCollapsed(`Physics Log (t=${this.clock.elapsedTime.toFixed(2)})`)
+          //   console.log('dt', dt)
+          //   console.log('speed', physics.velocity.length())
+          //   console.log('velocity', physics.velocity)
+          //   console.log('position', physics.position)
+          //   console.groupEnd()
+          // }
+        }
+      )
+    },
+
+    startAnimation() {
       this.clock.start()
 
       const animate = () => {
@@ -71,7 +130,7 @@ export const createApp = (canvas: HTMLCanvasElement): App => {
       animate()
     },
 
-    resize(this: App) {
+    resize() {
       const { innerWidth: w, innerHeight: h } = window
       const s = Math.min(w, h)
 
@@ -81,7 +140,7 @@ export const createApp = (canvas: HTMLCanvasElement): App => {
       renderer.setSize(s, s)
     },
 
-    render(this: App) {
+    render() {
       renderer.autoClear = true
 
       this.animatedScenes.forEach(as => {
@@ -96,16 +155,26 @@ export const createApp = (canvas: HTMLCanvasElement): App => {
     },
 
 
-    translate(this: App, dx: number, dy: number, sensitivity: number) {
-      cam.translateX(-dx * sensitivity)
-      cam.translateY(+dy * sensitivity)
+    // TODO: if ||<dx, dy>|| < threshold, decelerate
+    queueTranslation(this: App, dx: number, dy: number) {
+      // TODO: Hoist this to AppCameraDragger
+      const sensitivity = 10.0
+
+      // Convert (dx, dy) from screen axes to world axes
+      queuedTranslation = new THREE.Vector2(-dx, +dy)
+        .multiplyScalar(sensitivity)
     },
 
     // dz < 0: zoom in ;
     // dz > 0: zoom out
-    zoom(this: App, dz: number, sensitivity: number) {
-      const dzPersp = +dz * sensitivity + (dz < 0 ? 1.0 : 0.0)
-      cam.translateZ(dzPersp)
+    queueZoom(this: App, zoom: number) {
+      // TODO: Hoist this to AppCameraDragger
+      const sensitivity = 5.0
+
+      // Right-most factor prevents the result from being in the range (-s, +s)
+      //  where s is the sensitivity.
+      //  When |s| < 1, without the factor we will zoom in no matter what.
+      queuedZoom = +zoom * sensitivity + (zoom < 0.0 ? 1.0 : 0.0)
     }
   }
 }
