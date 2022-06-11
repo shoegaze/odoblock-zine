3cf47869fd4b98a32590d7154aaee79d8c9cb3cc
spike <bank.tellr@gmail.com>
2022-06-12 02:10:36 +0900

    Refactors `App`

--- src/App.ts
+++ src/App.ts
@@ -6,15 +6,15 @@ type AppAnimatedSceneMethod = (this: App, scene: AnimatedScene) => void
 type AppMethod = (this: App) => void
 
 export interface App {
-  cam: THREE.PerspectiveCamera,
-  renderer: THREE.WebGLRenderer,
-  animatedScenes: Array<AnimatedScene>,
-  clock: THREE.Clock,
+  cam: THREE.PerspectiveCamera
+  renderer: THREE.WebGLRenderer
+  animatedScenes: Array<AnimatedScene>
+  clock: THREE.Clock
 
   addScene: AppAnimatedSceneMethod
-  startAnimation: AppMethod,
-  resize: AppMethod,
-  render: AppMethod,
+  startAnimation: AppMethod
+  resize: AppMethod
+  render: AppMethod
 
   translate: (dx: number, dy: number, sensitivity: number) => void,
   zoom: (dz: number, sensitivity: number) => void
@@ -30,14 +30,6 @@ export const createApp = (canvas: HTMLCanvasElement): App => {
     1.0,        // aspect
     0.001, 1000 // near, far
   )
-
-  // Orthographic camera:
-  // const s_2 = s / 2
-  // const cam = new THREE.OrthographicCamera(
-  //   -s_2, +s_2, // left, right
-  //   +s_2, -s_2, // top, bottom
-  //   0.1, 1000   // near, far
-  // )
 
   cam.position.z = 500.0
 
@@ -83,18 +75,9 @@ export const createApp = (canvas: HTMLCanvasElement): App => {
       const { innerWidth: w, innerHeight: h } = window
       const s = Math.min(w, h)
 
-      // Perspective camera:
       cam.aspect = 1.0
-
-
-      // Orthographic camera:
-      // const s_2 = s / 2
-      // this.cam.left = -s_2
-      // this.cam.right = +s_2
-      // this.cam.top = +s_2
-      // this.cam.bottom = -s_2
-
       cam.updateProjectionMatrix()
+
       renderer.setSize(s, s)
     },
 
@@ -121,20 +104,8 @@ export const createApp = (canvas: HTMLCanvasElement): App => {
     // dz < 0: zoom in ;
     // dz > 0: zoom out
     zoom(this: App, dz: number, sensitivity: number) {
-      // Perspective camera:
       const dzPersp = +dz * sensitivity + (dz < 0 ? 1.0 : 0.0)
       cam.translateZ(dzPersp)
-
-      // Orthographic camera:
-      // const dzOrtho = -dz * sensitivity + (dz < 0 ? 1.0 : 0.0)
-      // this.animatedScenes.forEach(as => {
-      //   as.scene.children.forEach(obj => {
-      //     const sz = obj.scale.multiplyScalar(dzOrtho)
-
-      //     obj.scale.setX(sz.x)
-      //     obj.scale.setY(sz.y)
-      //   })
-      // })
     }
   }
 }
