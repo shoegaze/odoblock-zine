6eddcd88cf99daa36853129dc52253201005776e
spike <bank.tellr@gmail.com>
2022-06-20 10:20:02 +0900

    Really refactors `Layer` functions names

--- src/app/collection/layer/Layer.ts
+++ src/app/collection/layer/Layer.ts
@@ -1,5 +1,4 @@
-import { AppScene } from "./scene/AppScene"
-import { AnimatedScene } from "./scene/AnimatedScene"
+import { AppScene } from "../scene/AppScene"
 
 
 export const fadeDistance = 125.0
@@ -13,14 +12,14 @@ export interface Layer {
   setActive(this: Layer, active: boolean): void
 }
 
-export const toZPos = (id: number): number => -id * layersDistance
+export const idToZPos = (id: number): number => -id * layersDistance
 // https://graphtoy.com/?f1(x,t)=4&v1=false&f2(x,t)=abs(min(0.0,floor(x/f1(x))))&v2=true&f3(x,t)=f2(-x)%20*%20f1(x)&v3=true&f4(x,t)=&v4=false&f5(x,t)=&v5=false&f6(x,t)=&v6=false&grid=1&coords=-9.186638050037578,11.132397938736545,41.42725457271732
-export const toId = (zPos: number): number => Math.abs(Math.min(0, Math.floor(zPos / layersDistance)))
+export const zPosToId = (zPos: number): number => Math.abs(Math.min(0, Math.floor(zPos / layersDistance)))
 
 let idLayer = 0
 export const createLayer = (...scenes: AppScene[]): Layer => ({
   id: idLayer++,
-  zPos: toZPos(idLayer - 1),
+  zPos: idToZPos(idLayer - 1),
   scenes,
 
   setActive(active: boolean) {
