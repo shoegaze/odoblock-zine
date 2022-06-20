7aa73d74edbe7e5a29b3aa6e7cfec4ae178f9d8e
spike <bank.tellr@gmail.com>
2022-06-20 10:40:57 +0900

    Creates `Thread` stub

---
+++ src/app/collection/thread/Thread.ts
@@ -0,0 +1,44 @@
+import { Layer } from "../layer/Layer"
+
+
+export interface Thread {
+  layers: Layer[]
+}
+
+export const createThread = (...layers: Layer[]): Thread => ({
+  layers: layers.sort((l, r) => l.id - r.id)
+})
+
+type LayersGroupKey = string
+type LayersGroup = {
+  [key: LayersGroupKey]: Layer[]
+}
+
+type ThreadLayersGroupBy = (grouper: (layer: Layer) => LayersGroupKey, threads: Thread[]) => LayersGroup
+export const threadLayersGroupBy: ThreadLayersGroupBy = (grouper, threads) => {
+  let layers: LayersGroup = {}
+
+  threads.forEach(thread => {
+    const grouped = thread.layers.reduce((acc, layer) => {
+      const group = grouper(layer)
+
+      if (!acc[group]) {
+        acc[group] = []
+      }
+
+      acc[group].push(layer)
+      return acc
+    }, {} as LayersGroup)
+
+    // Is this behavior what we want?
+    //  What happens when there are duplicate layer ids?
+    // layers = { ...layers, ...grouped }
+
+    // Or is this it?
+    for (const g in grouped) {
+      layers[g] = (layers[g] || []).concat(grouped[g])
+    }
+  })
+
+  return layers
+}
