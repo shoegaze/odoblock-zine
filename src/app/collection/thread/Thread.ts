949571219c7f96b882049700b03ead08418ecff4
spike <bank.tellr@gmail.com>
2022-06-23 01:23:33 +0900

    Adds `Thread.idBounds` prop

--- src/app/collection/thread/Thread.ts
+++ src/app/collection/thread/Thread.ts
@@ -3,42 +3,14 @@ import { Layer } from "../layer/Layer"
 
 export interface Thread {
   layers: Layer[]
+  idBounds: [number, number] // [start, end]
 }
 
 export const createThread = (...layers: Layer[]): Thread => ({
-  layers: layers.sort((l, r) => l.id - r.id)
+  layers,
+  idBounds: [
+    // TODO: Combine into one pass
+    layers.reduce((acc, layer) => Math.min(acc, layer.zId), +Infinity),
+    layers.reduce((acc, layer) => Math.max(acc, layer.zId), -Infinity)
+  ]
 })
-
-type LayersGroupKey = string
-type LayersGroup = {
-  [key: LayersGroupKey]: Layer[]
-}
-
-type ThreadGroupByLayers = (grouper: (layer: Layer) => LayersGroupKey, threads: Thread[]) => LayersGroup
-export const threadGroupByLayers: ThreadGroupByLayers = (grouper, threads) => {
-  let layers: LayersGroup = {}
-
-  threads.forEach(thread => {
-    const grouped = thread.layers.reduce((acc, layer) => {
-      const group = grouper(layer)
-
-      if (!acc[group]) {
-        acc[group] = []
-      }
-
-      acc[group].push(layer)
-      return acc
-    }, {} as LayersGroup)
-
-    // Is this behavior what we want?
-    //  What happens when there are duplicate layer ids?
-    // layers = { ...layers, ...grouped }
-
-    // Or is this it?
-    for (const g in grouped) {
-      layers[g] = (layers[g] || []).concat(grouped[g])
-    }
-  })
-
-  return layers
-}
