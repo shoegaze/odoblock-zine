9aa0248fc1f571b4de78f159029b39e6f41e779f
shoegaze <bank.tellr@gmail.com>
2022-06-25 03:29:39 +0900

    Adds types `ZID` and `ZPos`

--- src/app/collection/layer/Layer.ts
+++ src/app/collection/layer/Layer.ts
@@ -4,19 +4,22 @@ import { AppScene } from "../scene/AppScene"
 export const fadeDistance = 50.0
 export const layersDistance = 150.0
 
+export type ZID = number
+export type ZPos = number
+
 export interface Layer {
-  zId: number
-  zPos: number
+  zId: ZID
+  zPos: ZPos
   scenes: Array<AppScene>
 
   setActive(this: Layer, active: boolean): void
 }
 
-export const zIdToZPos = (id: number): number => -id * layersDistance
+export const zIdToZPos = (id: ZID): ZPos => -id * layersDistance
 // https://graphtoy.com/?f1(x,t)=4&v1=false&f2(x,t)=abs(min(0.0,floor(x/f1(x))))&v2=true&f3(x,t)=f2(-x)%20*%20f1(x)&v3=true&f4(x,t)=&v4=false&f5(x,t)=&v5=false&f6(x,t)=&v6=false&grid=1&coords=-9.186638050037578,11.132397938736545,41.42725457271732
-export const zPosToZid = (zPos: number): number => Math.abs(Math.min(0, Math.floor(zPos / layersDistance)))
+export const zPosToZid = (zPos: ZPos): ZID => Math.abs(Math.min(0, Math.floor(zPos / layersDistance)))
 
-export const createLocalLayer = (zId: number, scenes: AppScene[]): Layer => ({
+export const createLocalLayer = (zId: ZID, scenes: AppScene[]): Layer => ({
   zId,
   zPos: zIdToZPos(zId),
   scenes,
@@ -28,9 +31,8 @@ export const createLocalLayer = (zId: number, scenes: AppScene[]): Layer => ({
   }
 })
 
-let idPersistent = 0
 export const createPersistentLayer = (scenes: AppScene[]): Layer => ({
-  zId: idPersistent++,
+  zId: 0,
   zPos: 0.0,
   scenes,
 
